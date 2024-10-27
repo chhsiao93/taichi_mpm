2366b16273fd99bdc0d3e6b9f87e27522df79b4a
Cheng-Hsi Hsiao <chhsiao@c301-002.ls6.tacc.utexas.edu>
2024-10-27 01:20:22 -0500

    modified from_generator argument

--- huggingface_create_dataset.py
+++ huggingface_create_dataset.py
@@ -2,13 +2,13 @@ import numpy as np
 from datasets import Dataset
 # load mpm data
 def gen():
-    for i in range(10):
+    for i in range(1000):
         mpm_data = np.load(f'adam/train/trajectory{i}.npz', allow_pickle=True)
         trajectory = mpm_data[f'trajectory{i}']
         x = trajectory[0][:,:,0]
         y = trajectory[0][:,:,1]
-        v_x = x[:-1] - x[1:]
-        v_y = y[:-1] - y[1:]
+        v_x = x[1:] - x[:-1]
+        v_y = y[1:] - y[:-1] 
         mat = trajectory[1]
         n_particles = mat.shape[0]
         n_steps = x.shape[0]
@@ -17,5 +17,5 @@ def gen():
         dataset_dict = {'x': x, 'y': y, 'v_x': v_x, 'v_y': v_y, 'mat': mat, 'n_particles': n_particles, 'n_steps': n_steps, 'friction_angle': friction_angle}
         yield dataset_dict
 
-ds = Dataset.from_generator(gen)
+ds = Dataset.from_generator(gen, cache_dir='huggingface_cache') # by default is "~/.cache/huggingface/datasets", which will make the home directory on TACC explode. That's why I specify a folder for cache files.
 ds.push_to_hub('chhsiao93/mpm_sand_2d')
